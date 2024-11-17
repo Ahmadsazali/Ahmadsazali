e9211851020eed74c2feb5ab0535aecdd3f2265b
Ahmadsazali <ahmadarsyi088@gmail.com>
2024-11-17 11:09:09 +0800

    Add files via upload

---
+++ Prog6.py
@@ -0,0 +1,13 @@
+# Modifikasi Tugas 2
+
+# Input panjang dan lebar
+lebar = 19
+panjang = 24
+
+# Hitung panjang lebar
+luas = panjang * lebar
+
+# Output hasil
+print("nilai lebar adalah:", lebar)
+print("nilai panjang adalah:", panjang)
+print("Luas persegi panjang adalah:", luas)
